06865969391499f683d2df42578852c05ec08865
zhihaofans <zhihaofans@hotmail.com>
2016-07-03 21:44:05 +0800

    V0.2：加入谷歌、搜狗，在网页顶端有设置可以切换(设置保存在本地浏览器)

--- One_key_search_image.js
+++ One_key_search_image.js
@@ -1,34 +1,72 @@
- // ==UserScript==
+// ==UserScript==
 // @name         匿名版一键搜图
 // @namespace    http://zhihaofans.com
-// @version      0.1
-// @description  一键搜图，让盗图狗不再得意
+// @version      0.2
+// @description  一键搜图(让盗图狗不再得意，让你不再为祭品信息而烦恼)
 // @author       zhihaofans
 // @match        https://h.nimingban.com/f/*
 // @match        https://h.nimingban.com/t/*
 // @grant        none
+// @note         V0.1：初始版本，只有百度，不带设置功能
+// @note         V0.2：加入谷歌、搜狗，在网页顶端有设置可以切换(设置保存在本地浏览器)
 // ==/UserScript==
-var oksi_loaded=0;
-var oksi_li = '<li><strong>一键搜图</strong> | <a href="javascript:void(0)" id="oksi_html_button">手动开启</a></li>';
+var oksi_setting_show=0;
 $(document).ready(function () {
 	oksi_setting();
-	oksi_start();
-	$("a#oksi_html_button").click(function () {
-		if(oksi_loaded==0)
+	$("#oksi_html_search").hide();
+	if(localStorage.oksi_setting_autorun=="yes")
+	{
+		oksi_start();
+	}
+	$("#oksi_html_button_setting").click(function () {
+		if(oksi_setting_show==0)
 		{
-			oksi_start();
-			alert("OK!?");
+			$("#oksi_html_search").show();
+			oksi_setting_show=1;
 		}
 		else
 		{
-			alert("错误\n\n\n已启动");
+			$("#oksi_html_search").hide();
+			oksi_setting_show=0;
 		}
+
+
 	});
 });
 function oksi_setting()
 {
-	$('ul.uk-breadcrumb').append(oksi_li);
-
+	if (localStorage.oksi_setting_search == undefined)
+	{
+		localStorage.oksi_setting_search = "baidu";
+	}
+	if (localStorage.oksi_setting_autorun == undefined)
+	{
+		localStorage.oksi_setting_autorun = "yes";
+	}
+	
+	switch(localStorage.oksi_setting_search)
+	{
+	case "baidu":
+		var oksi_select = "<select id=\"oksi_html_search\" name=\"oksi_html_search\" onchange=\"oksi_search_change(this.value)\"><option value=\"baidu\" selected=\"selected\">baidu</option><option value=\"google\">google</option><option value=\"sogou\">sogou</option></select>";
+		break;
+	case "google":
+		var oksi_select = "<select id=\"oksi_html_search\" name=\"oksi_html_search\" onchange=\"oksi_search_change(this.value)\"><option value=\"baidu\">baidu</option><option value=\"google\" selected=\"selected\">google</option><option value=\"sogou\">sogou</option></select>";
+		break;
+	case "sogou":
+		var oksi_select = "<select id=\"oksi_html_search\" name=\"oksi_html_search\" onchange=\"oksi_search_change(this.value)\"><option value=\"baidu\">baidu</option><option value=\"google\">google</option><option value=\"sogou\" selected=\"selected\">sogou</option></select>";
+		break;
+	default:
+		var oksi_select = "<select id=\"oksi_html_search\" name=\"oksi_html_search\" onchange=\"oksi_search_change(this.value)\"><option value=\"baidu\">baidu</option><option value=\"google\">google</option><option value=\"sogou\">sogou</option></select>";
+	}
+	
+	var oksi_li = "<li><a href=\"javascript:void(0)\" id=\"oksi_html_button_setting\">一键搜图</a></li> " + oksi_select;
+	$("ul.uk-breadcrumb").append(oksi_li);
+}
+function oksi_search_change(_search)
+{
+	localStorage.oksi_setting_search = _search;
+	alert("设置完毕");
+	location.reload();
 }
 function oksi_start()
 {
@@ -38,12 +76,29 @@ function oksi_start()
 	var oksi_imgs_num_1=oksi_imgs_num-1;
 	var oksi_link = "";
 	var oksi_a_1=1;
+	var oksi_search_sogou="http://pic.sogou.com/ris?query=";
+	var oksi_search_baidu="http://image.baidu.com/n/pc_search?queryImageUrl=";
+	var oksi_search_google="https://www.google.com.hk/searchbyimage?image_url=";
+	switch(localStorage.oksi_setting_search)
+	{
+	case "baidu":
+		var oksi_search=oksi_search_baidu;
+		break;
+	case "google":
+		var oksi_search=oksi_search_google;
+		break;
+	case "sogou":
+		var oksi_search=oksi_search_sogou;
+		break;
+	default:
+		var oksi_search=oksi_search_baidu;
+	}
 	for(var oksi_a=0;oksi_a<=oksi_imgs_num_1;oksi_a++)
 	{
 		var oksi_img="";
 		oksi_img=$("a.h-threads-img-a:eq("+oksi_a+")");
 		oksi_a_1=oksi_a+1;
-		oksi_link="<a  target=\"_blank\" id=\"one_key_search_image_"+oksi_a_1+"\" href=\"http://image.baidu.com/n/pc_search?queryImageUrl="+oksi_img.attr("href")+"\">(←一键搜图)</a>";
+		oksi_link="<a  target=\"_blank\" id=\"one_key_search_image_"+oksi_a_1+"\" href=\""+oksi_search+oksi_img.attr("href")+"\">(←一键搜图)</a>";
 		oksi_img.prop("outerHTML",oksi_link+oksi_img.prop("outerHTML"));
 	}
 }
